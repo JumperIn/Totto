b58a377217faaf359cbc7ca2aeb5e5c1e38e178f
JumperIn <wplak@yandex.ru>
2018-06-11 13:57:49 +0700

    Добавлен для примера фейковый общий репозиторий. Метод и модель для баннера. Клиенту рендерятся баннеры как попало.

--- MyTotto.Web/Abstract/BaseController.cs
+++ MyTotto.Web/Abstract/BaseController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using MyTotto.Data.Abstract;
+using MyTotto.Data.Repositories;
 
 namespace MyTotto.Web.Abstract
 {
     public class BaseController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        protected ICommonRepository repository;
+
+        public BaseController(ICommonRepository repository)
         {
-            return View();
+            this.repository = repository;
         }
+
     }
 }
--- MyTotto.Web/Controllers/CatalogController.cs
+++ MyTotto.Web/Controllers/CatalogController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MyTotto.Controllers
+namespace MyTotto.Web.Controllers
 {
     public class CatalogController : Controller
     {
