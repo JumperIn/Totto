cc8cab03a5fe82bea2b1ed8fc64cddb790a8020c
Sashako54 <sashako54@yandex.ru>
2018-07-01 15:46:35 +0700

    слияние с totto-web main.js

--- MyTotto.Web/src/scripts/main.js
+++ MyTotto.Web/src/scripts/main.js
@@ -1,150 +1,135 @@
 'use strict'
 
 //карусель
-var x;
-if(window.matchMedia('(min-width: 0px)').matches){ x = 1; };
-if(window.matchMedia('(min-width: 576px)').matches){ x = 2; };
-if(window.matchMedia('(min-width: 768px)').matches){ x = 3; };
-if(window.matchMedia('(min-width: 992px)').matches){ x = 4; };
-if(window.matchMedia('(min-width: 1200px)').matches){ x = 4; };
+var numProductsDisp;
+if (window.matchMedia('(min-width: 0px)').matches) { numProductsDisp = 1; };
+if (window.matchMedia('(min-width: 576px)').matches) { numProductsDisp = 2; };
+if (window.matchMedia('(min-width: 768px)').matches) { numProductsDisp = 3; };
+if (window.matchMedia('(min-width: 992px)').matches) { numProductsDisp = 4; };
+if (window.matchMedia('(min-width: 1200px)').matches) { numProductsDisp = 4; };
 
-var getProductWidth = function() {
-	var elem = document.getElementsByClassName('main-content-products__item')[0];
-	return elem.offsetWidth;
+function getProductWidth() {
+    var elem = document.getElementsByClassName('products__item')[0];
+    return elem.offsetWidth;
 };
 
-var getLengthWrapper = function () {
-	var elem = document.getElementsByClassName('products-wrapper')[0];
-	return elem.children.length
+function getLengthWrapper() {
+    var elem = document.getElementsByClassName('products-wrapper')[0];
+    return elem.children.length
 }
-var z = getLengthWrapper();
-var shift = 0;
+var numProducts = getLengthWrapper();
+var shiftProducts = 0;
 
-var moveCarouselRight = function() {
-	var elem = document.getElementsByClassName('MoveCarouselRight')[0];
-	elem.onclick = function() {
-		var elem = document.getElementsByClassName('products-wrapper')[0];
-		elem.style.transform = 'translateX(0px)';
-		// if (shift == (z - x * (z / x - z % x / x))*getProductWidth()) {
-		if (shift == (z - x)*getProductWidth() || shift > (z - x)*getProductWidth()) {
-			shift = 0;
-			elem.style.transform = 'translateX(-' + +shift + 'px)';
-		} else {
-			shift = +shift + +getProductWidth();
-			elem.style.transform = 'translateX(-' + +shift + 'px)';
-			console.log(shift);
-		}
-	}
+function moveCarouselRight() {
+    var elem = document.getElementsByClassName('products-wrapper')[0];
+    //граничное условие, при котором необходимо вернуть карусель в начальное состояние
+    var lastProduct = (numProducts - numProductsDisp) * getProductWidth();
+    if (shiftProducts == lastProduct || shiftProducts > lastProduct) {
+        shiftProducts = 0;
+        elem.style.transform = 'translateX(-' + +shiftProducts + 'px)';
+    } else {
+        shiftProducts = +shiftProducts + +getProductWidth();
+        elem.style.transform = 'translateX(-' + +shiftProducts + 'px)';
+    }
 };
-moveCarouselRight();
+function moveCarouselRightEvent() {
+    var elem = document.getElementById('moveCarouselRight');
+    elem.addEventListener('click', moveCarouselRight);
+};
+moveCarouselRightEvent();
 
-var moveCarouselLeft = function() {
-	var elem = document.getElementsByClassName('MoveCarouselLeft')[0];
-	elem.onclick = function() {
-		var elem = document.getElementsByClassName('products-wrapper')[0];
-		elem.style.transform = 'translateX(0px)';
-		if (shift == 0 || shift < 0) {
-			// shift = (z - x * (z / x - z % x / x))*getProductWidth();
-			shift = (z - x)*getProductWidth();
-			elem.style.transform = 'translateX(-' + +shift + 'px)';
-		} else {
-		
-			shift = +shift - +getProductWidth();
-			elem.style.transform = 'translateX(-' + +shift + 'px)';
-			console.log(shift);
-		}
-	}
+function moveCarouselLeft() {
+    var elem = document.getElementsByClassName('products-wrapper')[0];
+    if (shiftProducts == 0 || shiftProducts < 0) {
+        shiftProducts = (numProducts - numProductsDisp) * getProductWidth();
+        elem.style.transform = 'translateX(-' + +shiftProducts + 'px)';
+    } else {
+        shiftProducts = +shiftProducts - +getProductWidth();
+        elem.style.transform = 'translateX(-' + +shiftProducts + 'px)';
+    }
 };
-moveCarouselLeft();
+function moveCarouselLeftEvent() {
+    var elem = document.getElementById('moveCarouselLeft');
+    elem.addEventListener('click', moveCarouselLeft);
+};
+moveCarouselLeftEvent();
 
 //анимация main-nav
 function setClasses() {
-	var itemsNav = document.getElementsByClassName('main-nav__item');
-	for (var i = 0; i < itemsNav.length; i++) {
-		itemsNav[i].addEventListener('click', function(event) {
-			for (var j = 0; j < itemsNav.length; j++) {
-				itemsNav[j].classList.remove('main-nav__item_active');
-			}
-			event.target.classList.add('main-nav__item_active');
-		})
-	}
+    var itemsNav = document.getElementsByClassName('main-nav__item');
+    for (var i = 0; i < itemsNav.length; i++) {
+        itemsNav[i].addEventListener('click', function (event) {
+            for (var j = 0; j < itemsNav.length; j++) {
+                itemsNav[j].classList.remove('main-nav__item_active');
+            }
+            event.target.classList.add('main-nav__item_active');
+        })
+    }
 }
 setClasses();
 
 //слайдер баннера
-var getBannerWidth = function() {
-	var elem = document.getElementsByClassName('banners-wrapper')[0];
-	return elem.offsetWidth;
+function getBannerWidth() {
+    var elem = document.getElementsByClassName('banners-wrapper')[0];
+    return elem.offsetWidth;
 };
-var getBannersLengthWrapper = function () {
-	var elem = document.getElementsByClassName('banners-wrapper')[0];
-	return elem.children.length
+function getBannersLengthWrapper() {
+    var elem = document.getElementsByClassName('banners-wrapper')[0];
+    return elem.children.length
 }
 var numBanners = getBannersLengthWrapper();
 var shiftBanners = 0;
 
 function moveBannerRight() {
-	var elem = document.getElementsByClassName('banners-wrapper')[0];
-	elem.style.transform = 'translateX(0px)';
-	// if (shift == (z - x * (z / x - z % x / x))*getProductWidth()) {
-	if (shiftBanners == (numBanners - 1)*getBannerWidth() || shiftBanners > (numBanners - 1)*getBannerWidth()) {
-		shiftBanners = 0;
-		elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
-	} else {
-		shiftBanners = +shiftBanners + +getBannerWidth();
-		elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
-		console.log(shiftBanners);
-	}
+    var elem = document.getElementsByClassName('banners-wrapper')[0];
+    if (shiftBanners == (numBanners - 1) * getBannerWidth() || shiftBanners > (numBanners - 1) * getBannerWidth()) {
+        shiftBanners = 0;
+        elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
+    } else {
+        shiftBanners = +shiftBanners + +getBannerWidth();
+        elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
+    }
 }
 
-var moveBannerRightEvent = function() {
-	var elem = document.getElementsByClassName('banners-button-right')[0];
-	elem.onclick = function() {
-		moveBannerRight();
-	}
+function moveBannerRightEvent() {
+    var elem = document.getElementById('moveBannerRight');
+    elem.addEventListener('click', moveBannerRight);
 };
 moveBannerRightEvent();
 
 function moveBannerLeft() {
-	var elem = document.getElementsByClassName('banners-wrapper')[0];
-	elem.style.transform = 'translateX(0px)';
-	if (shiftBanners == 0 || shiftBanners < 0) {
-		// shift = (z - x * (z / x - z % x / x))*getProductWidth();
-		shiftBanners = (numBanners - 1)*getBannerWidth();
-		elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
-	} else {
-	
-		shiftBanners = +shiftBanners - +getBannerWidth();
-		elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
-		console.log(shiftBanners);
-	}
+    var elem = document.getElementsByClassName('banners-wrapper')[0];
+    if (shiftBanners == 0 || shiftBanners < 0) {
+        shiftBanners = (numBanners - 1) * getBannerWidth();
+        elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
+    } else {
+
+        shiftBanners = +shiftBanners - +getBannerWidth();
+        elem.style.transform = 'translateX(-' + +shiftBanners + 'px)';
+    }
 }
 
-var moveBannerLeftEvent = function() {
-	var elem = document.getElementsByClassName('banners-button-left')[0];
-	elem.onclick = function() {
-		moveBannerLeft();
-	}
+function moveBannerLeftEvent() {
+    var elem = document.getElementById('moveBannerLeft');
+    elem.addEventListener('click', moveBannerLeft);
 };
 moveBannerLeftEvent();
 
 //таймер слайда баннеров
-var slideBannerTimer = function() {
-	var slideBanner = setInterval(function() {moveBannerRight();}, 4000);
-	var stopMoveBanner = function() {
-		var elem = document.getElementsByClassName('banners')[0];
-		elem.addEventListener('mouseover', function(){
-			clearInterval(slideBanner);
-		});
-	}
-	stopMoveBanner();
+function slideBannerTimer() {
+    var slideBanner = setInterval(moveBannerRight, 4000);
+    var stopMoveBanner = function () {
+        var elem = document.getElementsByClassName('banners')[0];
+        elem.addEventListener('mouseover', function () {
+            clearInterval(slideBanner);
+        });
+    }
+    stopMoveBanner();
 }
 slideBannerTimer();
 
-var moveBannerMouseOut = function() {
-	var elem = document.getElementsByClassName('banners')[0];
-	elem.addEventListener('mouseout', function() {
-	slideBannerTimer();
-	})
+function moveBannerMouseOut() {
+    var elem = document.getElementsByClassName('banners')[0];
+    elem.addEventListener('mouseout', slideBannerTimer);
 }
 moveBannerMouseOut();
