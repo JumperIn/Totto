846bd82932ae86865f07b4795ae7b0574f9c371a
Sashako54 <sashako54@yandex.ru>
2018-07-31 16:51:57 +0700

    Адаптация навигации и сайта в целом

--- MyTotto.Web/src/scripts/main.js
+++ MyTotto.Web/src/scripts/main.js
@@ -14,9 +14,11 @@ function resize() {
 		timeOut = setTimeout(() => {
 			if (window.innerWidth > 0 && window.innerWidth < 768 && windowWidth > 767) {
 				console.log('от 0 до 767');
+				hideMenu();
 				windowWidth = window.innerWidth
-			} else if (window.innerWidth > 767 && window.innerWidth < 1366 && windowWidth < 768) {
-				console.log('от 767 до 1366');
+			} else if (window.innerWidth > 767 && window.innerWidth < 1920 && windowWidth < 768) {
+				console.log('от 767 до 1920');
+				showMenu();
 				windowWidth = window.innerWidth;
 			}
 			shiftProducts = 0;
@@ -24,9 +26,6 @@ function resize() {
 			getNumProductDisp();
 			returnBannerBack();
 			returnCarouselBack();
-
-			//внутри можно вызывать функции, которые определяют адаптивность сайта
-			//Можно так же вызывать функции, которые скажут, чтоб банеры и карусель прокрутились в начальное положение
 		}, 200)
 	})
 }
@@ -189,7 +188,7 @@ function dropDownMenu() {
 function adaptDropDownMenu() {
 	var elem = document.getElementsByClassName('header-nav-menu__link')[0];
     if (window.innerWidth < 768) {
-        elem.nextElementSibling.style.display = 'none'
+        elem.nextElementSibling.classList.toggle('hidden');
         elem.parentElement.parentElement.classList.remove('container_padding');
     }
 }
@@ -197,10 +196,10 @@ function dropDownMenuEvent() {
 	var elem = document.getElementsByClassName('header-nav-menu__link')[0];
 	elem.addEventListener('click', function () {
 		var panel = this.nextElementSibling;
-		if (panel.style.display === 'none') {
-			panel.style.display = 'flex';
+		if (!panel.classList.contains('hidden')) {
+			hideMenu();
 		} else {
-			panel.style.display = 'none';
+			panel.classList.toggle('hidden');
 		}
 	})
 
@@ -217,7 +216,7 @@ function adaptDropDownList() {
 	var elems = document.getElementsByClassName('accordion');
     for (var i = 0; i < elems.length; i++) {
         if (window.innerWidth < 768) {
-            elems[i].parentElement.nextElementSibling.classList.toggle('hiden');
+            elems[i].parentElement.nextElementSibling.classList.toggle('hidden');
             elems[i].classList.remove('fa-angle-down');
             elems[i].classList.add('fa-plus');
 		};
@@ -229,12 +228,12 @@ function dropDownListEvent() {
     for (var i = 0; i < elems.length; i++) {
 		elems[i].addEventListener('click', function () {
             var panel = this.parentElement.nextElementSibling;
-            this.classList.toggle('icon-accordion-active');
-            this.parentElement.classList.toggle('accordion-active');
             if (window.innerWidth < 768) {
+				this.classList.toggle('icon-accordion-active');
+				this.parentElement.classList.toggle('accordion-active');
                 this.classList.toggle('fa-plus');
                 this.classList.toggle('fa-minus');
-				panel.classList.toggle('hiden');
+				panel.classList.toggle('hidden');
             }
         })
 	}
@@ -251,11 +250,11 @@ function adaptDropDownItem() {
 	var elems = document.getElementsByClassName('accordion-list');
     for (var i = 0; i < elems.length; i++) {
         if (window.innerWidth < 768) {
-            elems[i].parentElement.nextElementSibling.classList.toggle('hiden');
+            elems[i].parentElement.nextElementSibling.classList.toggle('hidden');
             elems[i].parentElement.parentElement.parentElement.parentElement.classList.remove('container_padding');
         };
         if (window.innerWidth > 767) {
-            elems[i].style.display = 'none';
+            elems[i].classList.toggle('hidden');
 		};
 	}
 }
@@ -267,8 +266,99 @@ function dropDownItemEvent() {
 			this.classList.toggle('fa-plus');
 			this.classList.toggle('fa-minus');
 			var panel = this.parentElement.nextElementSibling;
-			panel.classList.toggle('hiden');
+			panel.classList.toggle('hidden');
 		})
 	}
 }
 dropDownItem();
+
+function hideMenu() {
+	//menu
+	var elem = document.getElementsByClassName('header-nav-menu__link')[0];
+	if (!elem.nextElementSibling.classList.contains('hidden')) {
+		elem.nextElementSibling.classList.toggle('hidden');
+        elem.parentElement.parentElement.classList.remove('container_padding');
+	}
+	//list
+	hideList();
+	//item
+	hideItem();
+}
+
+function hideList() {
+	var elems = document.getElementsByClassName('accordion');
+    for (var i = 0; i < elems.length; i++) {
+		if (!elems[i].classList.contains('fa-plus')) {
+            elems[i].parentElement.classList.remove('accordion-active');
+            elems[i].parentElement.classList.add('accordion-active');
+            elems[i].classList.remove('icon-accordion-active');
+            elems[i].classList.add('icon-accordion-active');
+			elems[i].classList.remove('fa-minus');
+			elems[i].classList.add('fa-minus');
+			elems[i].classList.remove('fa-angle-down');
+		}
+		if (!elems[i].parentElement.nextElementSibling.classList.contains('hidden')) {
+			elems[i].parentElement.nextElementSibling.classList.toggle('hidden');
+            elems[i].parentElement.classList.toggle('accordion-active');
+            elems[i].classList.toggle('icon-accordion-active');
+			elems[i].classList.toggle('fa-plus');
+			elems[i].classList.toggle('fa-minus');
+			elems[i].classList.remove('fa-angle-down');
+		}
+	}
+}
+
+function hideItem() {
+	var elems = document.getElementsByClassName('accordion-list');
+    for (var i = 0; i < elems.length; i++) {
+		if (elems[i].classList.contains('hidden')) {
+			elems[i].classList.toggle('sub-accordion-active');
+			elems[i].classList.toggle('fa-minus');
+			elems[i].classList.toggle('fa-plus');
+			elems[i].classList.toggle('hidden');
+		}
+		if (!elems[i].parentElement.nextElementSibling.classList.contains('hidden')) {
+			elems[i].parentElement.nextElementSibling.classList.toggle('hidden');
+			elems[i].parentElement.parentElement.parentElement.parentElement.classList.remove('container_padding');
+			elems[i].classList.toggle('sub-accordion-active');
+			elems[i].classList.toggle('fa-plus');
+			elems[i].classList.toggle('fa-minus');
+		}
+	}
+}
+
+function showMenu() {
+	//menu
+	var elem = document.getElementsByClassName('header-nav-menu__link')[0];
+	elem.nextElementSibling.classList.remove('hidden');
+	elem.parentElement.parentElement.classList.add('container_padding');
+	//list
+	showList();
+	//item
+	showItem();
+}
+
+function showList() {
+	var elems = document.getElementsByClassName('accordion');
+    for (var i = 0; i < elems.length; i++) {
+		elems[i].parentElement.nextElementSibling.classList.remove('hidden');
+		elems[i].parentElement.classList.remove('accordion-active');
+		elems[i].classList.remove('icon-accordion-active');
+		elems[i].classList.remove('fa-plus');
+		elems[i].classList.remove('fa-minus');
+		elems[i].classList.add('fa-angle-down');
+	}
+}
+
+function showItem() {
+	var elems = document.getElementsByClassName('accordion-list');
+    for (var i = 0; i < elems.length; i++) {
+		elems[i].parentElement.nextElementSibling.classList.remove('hidden');
+		elems[i].parentElement.parentElement.parentElement.parentElement.classList.add('container_padding');
+		elems[i].classList.remove('sub-accordion-active');
+		elems[i].classList.remove('fa-plus');
+		elems[i].classList.remove('fa-minus');
+		elems[i].classList.add('fa-plus');
+		elems[i].classList.toggle('hidden');
+	}
+}
