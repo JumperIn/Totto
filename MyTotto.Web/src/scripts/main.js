fa2b46200e52a46b179a87176eb62ee0ece37a83
Sashako54 <sashako54@yandex.ru>
2018-08-03 22:38:10 +0700

    Добавил появление навигации при скролинге сразу с адаптацией

--- MyTotto.Web/src/scripts/main.js
+++ MyTotto.Web/src/scripts/main.js
@@ -29,9 +29,11 @@ function resize() {
 			//из области от (0 до 768) в область (>= 768) и обратно.
 			if (window.innerWidth > 0 && window.innerWidth < constants.SCREEN_MEDIUM && windowWidth >= constants.SCREEN_MEDIUM) {
 				hideMenu();
-				windowWidth = window.innerWidth
+				hideDropNavAdaptive();
+				windowWidth = window.innerWidth;
 			} else if (window.innerWidth >= constants.SCREEN_MEDIUM && window.innerWidth < 1920 && windowWidth < constants.SCREEN_MEDIUM) {
 				showMenu();
+				scrollNavEvent();
 				windowWidth = window.innerWidth;
 			}
 			//для нормального отображения банеров и карусели, при изменении ширины экрана они плавно возвращаются в начальное положение
@@ -191,6 +193,89 @@ function moveBannerMouseOut() {
     elem.addEventListener('mouseout', slideBannerTimer);
 }
 moveBannerMouseOut();
+
+//scroll-navigation
+
+function scrollNavEvent() {
+	var elem = document.getElementsByClassName('header')[0];
+	var heightToggle = 0;
+	window.onscroll = function() {
+		if (window.pageYOffset > elem.offsetHeight && heightToggle < elem.offsetHeight) {
+			heightToggle = window.pageYOffset;
+			showDropNav();
+			
+		} else if (window.pageYOffset < elem.offsetHeight && heightToggle > elem.offsetHeight) {
+			heightToggle = 0;
+			hideDropNav();
+		}
+	}
+}
+
+function showDropNav() {
+	toggleClass('header-info', 'header-info_fixed');
+	toggleClass('header-nav', 'header-nav_fixed');
+	toggleClass('header-info-logo', 'header-info-logo_padding');
+	toggleClass('header-info-search_flex', 'col-md-7');
+	toggleClass('header-info-search_flex', 'col-md-6');
+	toggleClass('header-info__icon-wrapper', 'hidden');
+	toggleNotExistClass('header-nav', 'hidden');
+	toggleClass('header-title', 'header-title_height');
+	toggleClass('header-info', 'header-info_shadow');
+}
+
+function hideDropNav() {
+	toggleClass('header-info', 'header-info_fixed');
+	toggleClass('header-nav', 'header-nav_fixed');
+	toggleClass('header-info-logo', 'header-info-logo_padding');
+	toggleClass('header-info-search_flex', 'col-md-7');
+	toggleClass('header-info-search_flex', 'col-md-6');
+	toggleClass('header-info__icon-wrapper', 'hidden');
+	toggleExistClass('header-nav', 'hidden');
+	toggleExistClass('header-info__icon', 'header-info__icon_active');
+	toggleClass('header-title', 'header-title_height');
+	toggleExistClass('header-info', 'header-info_shadow');	
+}
+
+function hideDropNavAdaptive() {
+	var elem = document.getElementsByClassName('header')[0];
+	if (window.pageYOffset > elem.offsetHeight) {
+		hideDropNav();
+	}
+	window.onscroll = {}
+}
+
+if (window.innerWidth >= constants.SCREEN_MEDIUM) {
+	scrollNavEvent();
+}
+
+function toggleClass(clss1, clss2) {
+	var elem = document.getElementsByClassName(clss1)[0];
+	elem.classList.toggle(clss2);
+}
+
+function toggleNotExistClass(clss1, clss2) {
+	var elem = document.getElementsByClassName(clss1)[0];
+	if (!elem.classList.contains(clss2)) {
+		elem.classList.toggle(clss2)
+	}
+}
+
+function toggleExistClass(clss1, clss2) {
+	var elem = document.getElementsByClassName(clss1)[0];
+	if (elem.classList.contains(clss2)) {
+		elem.classList.toggle(clss2)
+	}
+}
+
+function dropDownNav() {
+	var elem = document.getElementsByClassName('header-info__icon')[0];
+	elem.addEventListener('click', function() {
+		this.classList.toggle('header-info__icon_active');
+		toggleClass('header-nav', 'hidden');
+		toggleClass('header-info', 'header-info_shadow');
+	})
+}
+dropDownNav();
 
 //Адаптивность навигации
 
