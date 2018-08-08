0b7e33ec0bd615605c74aee8c67ea471bade4137
Sashako54 <sashako54@yandex.ru>
2018-08-08 20:03:43 +0700

    Адаптация футера

--- MyTotto.Web/src/scripts/main.js
+++ MyTotto.Web/src/scripts/main.js
@@ -29,10 +29,12 @@ function resize() {
 			if (window.innerWidth > 0 && window.innerWidth < constants.SCREEN_MEDIUM && windowWidth >= constants.SCREEN_MEDIUM) {
 				hideMenu();
 				hideDropNavAdaptive();
+				adaptFooter();
 				windowWidth = window.innerWidth;
 			} else if (window.innerWidth >= constants.SCREEN_MEDIUM && window.innerWidth < 1920 && windowWidth < constants.SCREEN_MEDIUM) {
 				showMenu();
 				scrollNavEvent();
+				adaptFooter();
 				windowWidth = window.innerWidth;
 			}
 			//для нормального отображения банеров и карусели, при изменении ширины экрана они плавно возвращаются в начальное положение
@@ -457,3 +459,66 @@ function dropDownInfoItems() {
 	}
 }
 dropDownInfoItems();
+
+//Адаптация футера
+
+function hideFooterItems() {
+	var elems = document.getElementsByClassName('footer-block__body');
+	for (var i = 0; i < elems.length; i++) {
+		toggleNotExistClass('footer-block__body', 'hidden', i);
+	}
+}
+
+function showFooterItems() {
+	var elems = document.getElementsByClassName('footer-block__body');
+	for (var i = 0; i < elems.length; i++) {
+		toggleExistClass('footer-block__body', 'hidden', i);
+	}
+}
+
+function hideFooterIcons() {
+	var elems = document.getElementsByClassName('footer-block__body');
+	for (var i = 0; i < elems.length; i++) {
+		toggleNotExistClass('footer-block__icon', 'hidden', i)
+	}
+}
+
+function showFooterIcons() {
+	var elems = document.getElementsByClassName('footer-block__body');
+	for (var i = 0; i < elems.length; i++) {
+		toggleExistClass('footer-block__icon', 'hidden', i);
+	}
+}
+
+function toggleFooterIcons() {
+	var elems = document.getElementsByClassName('footer-block__icon');
+	for (var i = 0; i < elems.length; i++) {
+		toggleNotExistClass('footer-block__icon', 'fa-angle-down', i);
+		toggleExistClass('footer-block__icon', 'fa-angle-up', i);
+	}
+}
+
+function adaptFooter() {
+	if (window.innerWidth < constants.SCREEN_MEDIUM) {
+		hideFooterItems();
+		showFooterIcons();
+		toggleFooterIcons();
+	} else if (window.innerWidth >= constants.SCREEN_MEDIUM) {
+		showFooterItems();
+		hideFooterIcons();
+		toggleFooterIcons();
+	}
+}
+adaptFooter();
+
+function dropDownFooterItems() {
+	var elems = document.getElementsByClassName('footer-accordion');
+	for (var i = 0; i < elems.length; i++) {
+		elems[i].addEventListener('click', function() {
+			this.nextElementSibling.classList.toggle('hidden');
+			this.lastElementChild.classList.toggle('fa-angle-down');
+			this.lastElementChild.classList.toggle('fa-angle-up');
+		})
+	}
+}
+dropDownFooterItems();
