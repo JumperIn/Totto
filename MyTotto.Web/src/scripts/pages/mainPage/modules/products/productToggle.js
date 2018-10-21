3085b258c16b0a0f74afc17ab5eb3b9dc487465f
sashako54 <sashako54@yandex.ru>
2018-10-22 01:53:36 +0700

    Наладил отображение продуктов в ProductCard изменил js-products_{type} на js-products-{type}, т.к. обёрточные файлы продуктов уже имеют класс с таким названием

--- MyTotto.Web/src/scripts/pages/mainPage/modules/products/productToggle.js
+++ MyTotto.Web/src/scripts/pages/mainPage/modules/products/productToggle.js
@@ -1,28 +1,17 @@
 import { toggleExistClass, toggleNotExistClass } from "../../../../shared/const/toggleClass";
 
-function toggleProductsEventAll() {
-	var elem = document.getElementsByClassName('js-main-nav__item-all')[0];
-	var elems = document.getElementsByClassName('products__item');
-	elem.addEventListener('click', function() {
-		for ( var i = 0; i < elems.length; i++) {
-			toggleExistClass('products__item', 'hidden', i);
-		}
-	})
-}
-
 function toggleProductsEventItems(classButton, classProduct) {
 	var elem = document.getElementsByClassName(classButton)[0];
-	var elems1 = document.getElementsByClassName('products__item');
-	var elems2 = document.getElementsByClassName(classProduct);
+	var elems1 = document.getElementsByClassName('products');
 	elem.addEventListener('click', function() {
-		for ( var i = 0; i < elems1.length; i++) {
-			toggleNotExistClass('products__item', 'hidden', i);
-		}
-		// для ограничения, можно добавить в условие для && i < 8 чтоб, не выводить слишком много товаров
-		for ( var i = 0; i < elems2.length; i++) {
-			toggleExistClass(classProduct, 'hidden', i);
+		if (!elem.classList.contains('main-nav__item_active')) {
+			for ( var i = 0; i < elems1.length; i++) {
+				toggleNotExistClass('products', 'hidden', i);
+			}
+			// для ограничения, можно добавить в условие для && i < 8 чтоб, не выводить слишком много товаров
+			toggleExistClass(classProduct, 'hidden', 0);
 		}
 	})
 }
 
-export { toggleProductsEventAll, toggleProductsEventItems };
+export { toggleProductsEventItems };
