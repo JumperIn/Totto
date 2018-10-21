bf018498ae2d2a0531455866a8e674be3faf6bc6
JumperIn <wplak@yandex.ru>
2018-10-21 20:37:10 +0700

    Добавлены методы расширения для получения рандомного элемента. (#14)
    В модели представления для главной страницы появилась модель для всего списка продуктов.
    
    Появилось представление для карточки продукта.

---
+++ MyTotto.Web/Extensions/RandomElementExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTotto.Web.Extensions
+{
+    /// <summary>
+    /// Расширение для получения случайных элементов.
+    /// </summary>
+    public static class RandomElementExtension
+    {
+        private static Random random = new Random();
+
+        /// <summary>
+        /// Возвращает случайный элемент из списка.
+        /// </summary>
+        /// <typeparam name="T">Тип.</typeparam>
+        /// <param name="items">Список элементов.</param>
+        public static T RandomElement<T>(this IEnumerable<T> items)
+        {
+            int number = random.Next(items.Count());
+            return items.ElementAt(number);
+        }
+
+        /// <summary>
+        /// Возвращает коллекцию случайных элементов по количеству.
+        /// </summary>
+        /// <typeparam name="T">Тип.</typeparam>
+        /// <param name="items">Список элементов.</param>
+        /// <param name="count">Количество элементов.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> RandomElements<T>(this IEnumerable<T> items, int count)
+        {
+            var list = new List<T>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(items.RandomElement());
+            }
+
+            return list;
+        }
+    }
+}
