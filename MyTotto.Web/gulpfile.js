9a995f6297a5526d1a2d73e98912e971f29f4fab
sashako54 <38761257+sashako54@users.noreply.github.com>
2018-12-25 01:00:01 +0700

    Добавил favicon иконки (#36)
    иконки разных размеров для ios, android и т.д.

--- MyTotto.Web/gulpfile.js
+++ MyTotto.Web/gulpfile.js
@@ -1,12 +1,12 @@
 ﻿var gulp = require("gulp"),
-    fs = require("fs"),
-    babel = require("gulp-babel"),
-    cssmin = require('gulp-cssmin'),
-    gulpif = require('gulp-if'),
-    argv = require('yargs').argv,
-    stylus = require("gulp-stylus"),
-    gutil = require('gulp-util'),
-    concat = require('gulp-concat'),
+	fs = require("fs"),
+	babel = require("gulp-babel"),
+	cssmin = require('gulp-cssmin'),
+	gulpif = require('gulp-if'),
+	argv = require('yargs').argv,
+	stylus = require("gulp-stylus"),
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
 	livereload = require('gulp-livereload'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
@@ -26,28 +26,28 @@ var source = 'src';
 
 // сюда указываем пути у подключаемых библиотек
 var libsCss = [
-    `./${source}/libs/styles/bootstrap-grid.min.css`,
-    `./${source}/libs/styles/normalize.css`,
+	`./${source}/libs/styles/bootstrap-grid.min.css`,
+	`./${source}/libs/styles/normalize.css`,
 ];
 
 
 // сюда указываем пути у подключаемых библиотек
 var libsJs = [
-    `./${source}/libs/scripts/jquery.min.js`
+	`./${source}/libs/scripts/jquery.min.js`
 ];
 
 var srcPaths = {
-    stylus: [`./${source}/css/site.styl`],
-    img: [`./${source}/img/**`],
-    icons: [`./${source}/icons/**`],
+	stylus: [`./${source}/css/site.styl`],
+	img: [`./${source}/img/**`],
+	icons: [`./${source}/icons/**`],
 	js: [
 		`./${source}/scripts/pages/mainPage/*.js`,
 		// `./${source}/scripts/pages/somePage/*.js`
 	],
-    favicon: [`./${source}/favicon.ico`],
-    fonts: [`./${source}/fonts/**`],
-    libsCss: libsCss,
-    libsJs: libsJs,
+	favicon: [`./${source}/favicon/**`],
+	fonts: [`./${source}/fonts/**`],
+	libsCss: libsCss,
+	libsJs: libsJs,
 	allStylus: [`./${source}/css/*.styl`],
 	svgIcons: [`./${source}/libs/styles/svg-icons/*.svg`]
 }
@@ -55,17 +55,17 @@ var srcPaths = {
 // пути для сборки
 var destination = 'wwwroot';
 var destPaths = {
-    css: `./${destination}/css`,
-    img: `./${destination}/images`,
-    icons: `./${destination}/icons`,
-    js: `./${destination}/js`,
-    favicon: `./${destination}`,
-    fonts: `./${destination}/fonts`,
-    libFolderJs: `./${destination}/libs/scripts`,
-    libFileJs: 'script.js',
-    libFolderCss: `./${destination}/libs/styles`,
+	css: `./${destination}/css`,
+	img: `./${destination}/images`,
+	icons: `./${destination}/icons`,
+	js: `./${destination}/js`,
+	favicon: `./${destination}`,
+	fonts: `./${destination}/fonts`,
+	libFolderJs: `./${destination}/libs/scripts`,
+	libFileJs: 'script.js',
+	libFolderCss: `./${destination}/libs/styles`,
 	libFileCss: 'style.css',
-	svgIcons: `./Views/Shared`,	
+	svgIcons: `./Views/Shared`,
 	// svgIcons: `./${destination}/libs/svg-icons`	
 }
 
@@ -73,13 +73,13 @@ var production = argv.production !== undefined || argv.prod !== undefined;
 
 // сборка стилей
 gulp.task("stylus", function () {
-    return gulp.src(srcPaths.stylus)
+	return gulp.src(srcPaths.stylus)
 		.pipe(stylus())
 		.pipe(gulp.dest(destPaths.css));
-		// .pipe(cssmin())
-		// .pipe(rename({ suffix: '.min' }))
-		// .pipe(gulp.dest(destPaths.css));
-	});
+	// .pipe(cssmin())
+	// .pipe(rename({ suffix: '.min' }))
+	// .pipe(gulp.dest(destPaths.css));
+});
 
 // сборка скриптов
 gulp.task("js", function () {
@@ -109,43 +109,43 @@ gulp.task("js", function () {
 			mode: 'none',
 		}))
 		.pipe(gulp.dest(destPaths.js));
-		// .pipe(uglify())
-		// .pipe(rename({ suffix: '.min' }))
-		// .pipe(gulp.dest(destPaths.js));
+	// .pipe(uglify())
+	// .pipe(rename({ suffix: '.min' }))
+	// .pipe(gulp.dest(destPaths.js));
 });
 
 // копирование ресурсов
 gulp.task('copy', function () {
-    gulp.src(srcPaths.icons)
-        .pipe(gulp.dest(destPaths.icons));
+	gulp.src(srcPaths.icons)
+		.pipe(gulp.dest(destPaths.icons));
 	gulp.src(srcPaths.img)
 		.pipe(imagemin([mozjpeg({
 			quality: 85
 		})]))
-        .pipe(gulp.dest(destPaths.img));
-    gulp.src(srcPaths.fonts)
-        .pipe(gulp.dest(destPaths.fonts));
+		.pipe(gulp.dest(destPaths.img));
+	gulp.src(srcPaths.fonts)
+		.pipe(gulp.dest(destPaths.fonts));
 });
 
 gulp.task('libs', function () {
-    gulp.src(srcPaths.libsJs)
-        .pipe(concat(destPaths.libFileJs))
+	gulp.src(srcPaths.libsJs)
+		.pipe(concat(destPaths.libFileJs))
 		.pipe(gulp.dest(destPaths.libFolderJs));
-		// .pipe(uglify())
-		// .pipe(rename({ suffix: '.min'}))
-		// .pipe(gulp.dest(destPaths.libFolderJs));
+	// .pipe(uglify())
+	// .pipe(rename({ suffix: '.min'}))
+	// .pipe(gulp.dest(destPaths.libFolderJs));
 
-    gulp.src(srcPaths.libsCss)
+	gulp.src(srcPaths.libsCss)
 		.pipe(concat(destPaths.libFileCss))
-        .pipe(gulp.dest(destPaths.libFolderCss));
-		// .pipe(cssmin())
-		// .pipe(rename({ suffix: '.min' }))
-        // .pipe(gulp.dest(destPaths.libFolderCss));
+		.pipe(gulp.dest(destPaths.libFolderCss));
+	// .pipe(cssmin())
+	// .pipe(rename({ suffix: '.min' }))
+	// .pipe(gulp.dest(destPaths.libFolderCss));
 });
 
 gulp.task('favicon', function () {
-    gulp.src(srcPaths.favicon)
-        .pipe(gulp.dest(destPaths.favicon));
+	gulp.src(srcPaths.favicon)
+		.pipe(gulp.dest(destPaths.favicon));
 })
 
 gulp.task('svg', function () {
@@ -178,21 +178,21 @@ gulp.task('svg', function () {
 })
 
 gulp.task('watch', function () {
-    livereload.listen();
-    gulp.watch(
-        // [...srcPaths.allStylus, ...srcPaths.js, ...srcPaths.icons, ...srcPaths.img, ...srcPaths.libsJs, ...srcPaths.libsCss],
+	livereload.listen();
+	gulp.watch(
+		// [...srcPaths.allStylus, ...srcPaths.js, ...srcPaths.icons, ...srcPaths.img, ...srcPaths.libsJs, ...srcPaths.libsCss],
 		// [ 'stylus', 'js', 'copy', 'libs' ])
 		// убрал js из watch, так как watch есть в gulp js отдельно
-        [...srcPaths.allStylus, ...srcPaths.icons, ...srcPaths.img, ...srcPaths.libsJs, ...srcPaths.libsCss],
-        [ 'stylus', 'copy', 'libs' ])
-        .on('change', livereload.changed);
+		[...srcPaths.allStylus, ...srcPaths.icons, ...srcPaths.img, ...srcPaths.libsJs, ...srcPaths.libsCss],
+		['stylus', 'copy', 'libs'])
+		.on('change', livereload.changed);
 });
 
 gulp.task('default', [
-    'copy',
-    'libs',
-    'js',
-    'stylus',
+	'copy',
+	'libs',
+	'js',
+	'stylus',
 	'favicon',
 	'svg'
 ]);
