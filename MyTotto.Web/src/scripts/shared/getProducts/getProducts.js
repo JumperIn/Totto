38448180ccdebdbb2d740437f9885e35ad2d0dbe
sashako54 <sashako54@yandex.ru>
2019-01-10 02:56:17 +0700

    допилил визуальное отображение продуктов при перерисовке

--- MyTotto.Web/src/scripts/shared/getProducts/getProducts.js
+++ MyTotto.Web/src/scripts/shared/getProducts/getProducts.js
@@ -9,9 +9,25 @@ function drawProducts(products) {
 
 	for (let i = 0; i < products.length; i++) {
 		const template = document.querySelector('template.products__item-template');
-		const link = template.content.querySelector('a.product');
+		// const link = template.content.querySelector('a.product');
+		let image = template.content.querySelector('div.product__img');
+		const productName = template.content.querySelector('h4.product__name');
+		const price = template.content.querySelector('p.product__price-before');
+		const discountPrice = template.content.querySelector('p.product__price-after');
+		const productSale = template.content.querySelector('div.product-sale');
 
-		link.href = ``;
+		// link.href = ``;
+		image.style.backgroundImage = `url(${products[i].cardImage})`;
+		productName.innerHTML = `${products[i].manufacturer.title} ${products[i].title}`;
+		price.innerHTML = products[i].discount !== 0 ? products[i].price.toFixed(2).toString().replace('.', ',') : '';
+		discountPrice.innerHTML = products[i].discountPrice.toFixed(2).toString().replace('.', ',');
+		if (products[i].productType === 0) {
+			productSale.innerHTML = '<div class="js-product-sale__hit product-sale_orange">Хит</div>';
+		} else if (products[i].productType === 1) {
+			productSale.innerHTML = '<div class="js-product-sale__new product-sale_light-blue">New</div>';
+		} else if (products[i].productType === 2) {
+			productSale.innerHTML = '<div class="js-product-sale__stock product-sale_red">Акция</div>';
+		}
 
 		let clone = document.importNode(template.content, true);
 
